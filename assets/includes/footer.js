06288d4feea756d8324bc25fa7b45229a3f5fee2
Stefan Lasse <lasse.stefan@gmx.de>
2015-05-31 20:43:36 +0200

    initial commit

---
+++ assets/includes/footer.js
@@ -0,0 +1,8 @@
+document.write('\
+<!-- Copyright -->\
+  <div class="copyright">\
+    <ul class="menu">\
+      <li>&copy; LK Instruments. All rights reserved.</li><li><a href="imprint.html">Imprint</a></li><li>Design: <a href="http://html5up.net" target="_blank">HTML5 UP</a></li>\
+    </ul>\
+  </div>\
+');
---
+++ assets/includes/navigation.js
@@ -0,0 +1,58 @@
+document.write('\
+<!-- Header -->\
+<div id="header">\
+\
+<div class="row 100%">\
+  <section class="4u 12u(narrower)">\
+  </section>\
+  <section class="4u 12u(narrower)">\
+    <h1><img src="./images/RZ_LK_Instruments_test.svg" height="80">\
+  </section>\
+  <section class="4u 12u(narrower)">\
+      <h1><a href="index.html" id="logo">Better gear. <em>Better science.</em></a></h1>\
+  </section>\
+</div>\
+\
+  <!-- Logo -->\
+\
+  <!-- Nav -->\
+  <nav id="nav">\
+    <ul>\
+      <li><a href="index.html">Home</a></li>\
+      <li><a href="news.html">News</a></li>\
+      <li>\
+        <a href="products.html">Products</a>\
+        <ul>\
+          <li><a href="smc.html">Stepper Motor Controllers</a></li>\
+          <li><a href="motors.html">Opto mechanics</a></li>\
+          <li><a href="#">Photodiodes</a></li>\
+          <li><a href="#">Universal Boards</a></li>\
+            <!--\
+            <li>\
+            <a href="#">Submenu</a>\
+            <ul>\
+              <li><a href="#">Lorem dolor</a></li>\
+              <li><a href="#">Phasellus magna</a></li>\
+              <li><a href="#">Magna phasellus</a></li>\
+              <li><a href="#">Etiam nisl</a></li>\
+              <li><a href="#">Veroeros feugiat</a></li>\
+            </ul>\
+            </li>\
+            -->\
+        </ul>\
+      </li>\
+    <li><a href="company.html">Company</a></li>\
+    <li><a href="contact.html">Contact</a></li>\
+  </ul>\
+</nav>\
+\
+</div>\
+');
+
+
+
+
+
+
+
+
