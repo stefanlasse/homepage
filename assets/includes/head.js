9eadb9d30e95cb64492ceb092422970c42de7d8d
Stefan Lasse <lasse.stefan@gmx.de>
2015-06-02 12:55:52 +0200

    contact forms on every page

---
+++ assets/includes/head.js
@@ -0,0 +1,22 @@
+document.write('\
+<meta charset="utf-8" />\
+		<meta name="viewport" content="width=device-width, initial-scale=1" />\
+    <meta name="author" content="LK-Instruments" />\
+    <meta name="description" content="Precision positioning equipment for scientific laboratories, universities and industry." />\
+    <meta name="keywords" content="positioning, rotary stages, polarization adjustment, stepper motor control, LK-instruments, LK instruments, motor driver" />\
+    <meta name="robots" content="index" />\
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->\
+		<link rel="stylesheet" href="assets/css/main.css" />\
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->\
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->\
+		\
+		<link rel="shortcut icon" href="./images/icon.ico">\
+');
+
+
+
+
+
+
+
+
